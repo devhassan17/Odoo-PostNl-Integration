4eadf501649c040921daa736dd86193cc27b2183
devhassan17 <156808621+devhassan17@users.noreply.github.com>
2025-12-29 22:30:01 +0500

    Update

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from . import postnl_order_log
 from . import postnl_shipping_rule
 from . import postnl_config
 from . import res_config_settings
-from. import stock_picking
+from . import stock_picking
+from . import postnl_replenishment
 from .stock_picking import StockPicking
 from .postnl_replenishment import PostNLReplenishment
