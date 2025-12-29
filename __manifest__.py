b6e11d5c38656e00905f32143f5afd7fb2470d1d
devhassan17 <156808621+devhassan17@users.noreply.github.com>
2025-12-30 00:31:52 +0500

    Update

--- __manifest__.py
+++ __manifest__.py
@@ -5,23 +5,35 @@
     'version': '18.0.1.0.0',
     'category': 'Sales',
     'images': ['static/description/icon.png'],
-    'author': 'Ali Hassan15',
-    'depends': ['sale_management'],
-    'data': [
-    'security/ir.model.access.csv',
+    'author': 'Ali Hassan16',
+    'license': 'LGPL-3',
 
-    # views first (actions)
-    'views/postnl_order_log_views.xml',
-    'views/postnl_config_views.xml',
-    "views/postnl_replenishment_views.xml",
-    # menus LAST
-    'views/postnl_menu.xml',
-],
+    'depends': [
+        'sale_management',
+        'stock',   # IMPORTANT: picking / carrier_tracking_ref
+        'base',
+    ],
+
+    'data': [
+        # 🔐 Security
+        'security/ir.model.access.csv',
+
+        # ⏱️ Cron
+        'data/ir_cron.xml',
+
+        # 👁️ Views (actions first)
+        'views/postnl_order_log_views.xml',
+        'views/postnl_config_views.xml',
+        'views/postnl_replenishment_views.xml',
+
+        # 📂 Menus LAST
+        'views/postnl_menu.xml',
+    ],
 
     'installable': True,
     'application': True,
     'auto_install': False,
-    "license": "LGPL-3",
+
     'external_dependencies': {
         'python': ['requests'],
     },
---
+++ controllers/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import postnl_fulfilment_receiver
