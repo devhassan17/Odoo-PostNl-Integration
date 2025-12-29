9ee94a4cad47427c6382120a1a149dd5982a2934
devhassan17 <156808621+devhassan17@users.noreply.github.com>
2025-12-29 22:21:07 +0500

    Updated

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'version': '18.0.1.0.0',
     'category': 'Sales',
     'images': ['static/description/icon.png'],
-    'author': 'Ali Hassan9',
+    'author': 'Ali Hassan10',
     'depends': ['sale_management'],
     'data': [
     'security/ir.model.access.csv',
